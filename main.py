4e584bbfa7f47a89d63c733e51baad1b506e555d
Croitoru-Alexandru <91969171+Croitoru-Alexandru@users.noreply.github.com>
2021-10-12 20:51:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,16 @@ Funcția de test: test_is_superprime()
 '''
 
 def is_prime(n):
+    '''
+    is_prime determina daca un numar este prim sau nu
+    :param n: este numar intreg
+    :return: True daca este prim si False daca nu este prim
+    '''
     if n < 2:
         return False
     for i in range(2, n//2+1):
         if n % i == 0:
             return False
-        else:
-            return True
     return True
 
 def test_is_prime():
@@ -25,23 +28,29 @@ test_is_prime()
 
 
 def is_superprime(n):
+    '''
+    determina daca un numar este superprim
+    :param n: este numar intreg
+    :return: True daca este superprim,Fals daca nu este superprim
+    '''
+    ok = 1
     while(n):
-#        if n == 237:
-#           return False
-#        elif is_prime(n) == True:
-#           return True
-        if is_prime(n) == True:
-            return True
-        else:
-            return False
+        if is_prime(n) is False:
+            ok = 0
+        n = n // 10
+    if ok == 1:
+        return True
+    else:
+        return False
+
+        
 
 def test_is_superprime():
     assert is_superprime(233) == True
-    assert is_superprime(239) == True
     assert is_superprime(240) == False
-    assert is_superprime(953) == True
+    assert is_superprime(953) == False
     assert is_superprime(740) == False
-#   assert is_superprime(237) == False
+    assert is_superprime(237) == False
 test_is_superprime()
 '''
 7. Determină dacă un număr este antipalindrom: un număr este antipalindrom dacă oricare două cifre egal depărtate de extremități sunt diferite 
@@ -76,3 +85,32 @@ def test_is_antipalindrome():
     assert is_antipalindrome(2424) is True
     assert is_antipalindrome(2772) is False
 test_is_antipalindrome()
+
+option = '''
+Daca doriti sa aflati daca un numar este antipalindrom scrie 1.
+Daca doriti sa aflati daca un numar este superprim scrie 2.
+Daca doriti sa opriti programul scrie 3.
+'''
+
+def main():
+    while True:
+        optiune = input(option)
+        if optiune == '1':
+            numar = int(input("Scrieti valoarea:"))
+            if is_antipalindrome(numar):
+                print("Este antipalindrom!")
+            else:
+                print("Nu este antipalindrom!")
+        elif optiune == '2':
+            numar = input("Scrieti numarul: ")
+            if is_superprime(numar):
+                print("Este numar superprim!")
+            else:
+                print("Nu este un numar superprim!")
+        elif optiune == '3':
+            print("programul s-a oprit!")
+            break
+        else:
+            print("Nu exista comanda!")
+if __name__ == main():
+    main()
